c504c85534b8d1d66348d9c5aa09011246a9046b
Robed <robedbeauvil@gmail.com>
2020-11-14 17:56:27 -0500

    Updates

---
+++ precache-manifest.5cd05faa632c397858bd4ea5cbb0c1db.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a963c7b09383b5dfbeb0c005b556566a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "75a575281947d3ba00e4",
+    "url": "/portfolio/static/css/main.93cb52a1.chunk.css"
+  },
+  {
+    "revision": "961d4d27fb06ea9f2cac",
+    "url": "/portfolio/static/js/2.f2757a7d.chunk.js"
+  },
+  {
+    "revision": "7bc9b478038c6bedf8340d67bd512bd3",
+    "url": "/portfolio/static/js/2.f2757a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a575281947d3ba00e4",
+    "url": "/portfolio/static/js/main.837add93.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "1ab691bca72020f4641a19a4d2d6d697",
+    "url": "/portfolio/static/media/DSA.1ab691bc.jpg"
+  },
+  {
+    "revision": "4250a6eb252d16591f65ac7c8ee39a78",
+    "url": "/portfolio/static/media/TWITTY.4250a6eb.png"
+  },
+  {
+    "revision": "d5d26ca27bbe86955a4948bca243cff0",
+    "url": "/portfolio/static/media/calc_bot.d5d26ca2.png"
+  },
+  {
+    "revision": "ad4cad71f24f256b11a7175f55452a03",
+    "url": "/portfolio/static/media/learnSQL.ad4cad71.png"
+  },
+  {
+    "revision": "2ae1bdcc432ef1e053613349fb7113f6",
+    "url": "/portfolio/static/media/learnSQL2.2ae1bdcc.png"
+  },
+  {
+    "revision": "973dab1837b6912e2041ae45ef1ec479",
+    "url": "/portfolio/static/media/resume.973dab18.png"
+  },
+  {
+    "revision": "dc5546a557596cd6235e618951c04d26",
+    "url": "/portfolio/static/media/sFITNESS.dc5546a5.jpg"
+  }
+]);
