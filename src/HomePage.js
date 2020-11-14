1a8beaf6f80a468df6c4cbee8a679f3bf2575144
Robed <robedbeauvil@gmail.com>
2020-11-13 22:21:35 -0500

    simplified homepage file

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,126 +1,13 @@
 import React from "react"
-import Thumbnail from './Thumbnail.js';
-import Summary from './Summary.js';
 import ContactMe from './ContactMe.js'
 import AboutMe from './AboutMe.js';
-import Icon from './Images/ThumbnailImage';
-import sFITNESS from './Images/sFITNESS.jpg'
-import calc_bot from './Images/calc_bot.png'
-import learnSQL from './Images/learnSQL.png'
-import learnSQL2 from './Images/learnSQL2.png'
-import TWITTY from './Images/TWITTY.png'
-import DSA from './Images/DSA.jpg'
+import Projects from './Projects.js'
 
 function HomePage(props) {
   return (
     <div>
         <AboutMe />
-        <div className="w3-row w3-animate-left container2">
-        <div className="w3-half w3-blue-grey and w3-center" id="work">
-            <div className="w3-padding-64 w3-animate-right">
-                <h1><strong>Projects</strong></h1>
-                    
-                    <Icon icon={calc_bot} />
-
-                    <Thumbnail
-                    link="https://calctutor123.herokuapp.com/"
-                    title="Calculus-Calculator"
-                    category="E-learning App"
-                    />
-
-                    <Summary
-                    link="https://github.com/beauvilerobed/calctutor123"
-                    title="Artificial math tutor that helps you find derivatives  and indefinite integrals through a step-by-step process with optional c."
-                    category="Python3, HTML/CSS, Javascript, Django, Bootstrap, D3.js, Keras, NLTK, Tensorflow, JSON, Sympy, Numpy"
-                    />
-
-                    <Icon icon={TWITTY} />
-                    
-                    <Thumbnail
-                    link="https://twityclone.herokuapp.com"
-                    title="TWITY"
-                    category=" Microblogging App"
-                    />
-
-                    <Summary
-                    link="https://github.com/beauvilerobed/twity-app"
-                    title="Allow users to exchange small elements of content such as short sentences, short posts, quick thoughts ranging from followers to everyone."
-                    category="Python3, HTML/CSS, Javascript, Flask, Bootstrap, Jinja2, SQLAlchemy"
-                    />
-
-                    <Icon icon={sFITNESS} />
-                    
-                    <Thumbnail 
-                    link="http://sfitnessco.com"
-                    title="sFITNESS"
-                    category="Fitness and Wellness App"
-                    />
-
-                    <Summary
-                    link="https://github.com/beauvilerobed/sFITNESS"
-                    title="sFITNESS is an automated personal trainer that wants to make exercise  and wellness simple.
-                    Health  and fitness should not need to be difficult  and it should be sustainable."
-                    category="PHP, MySQL, CSS, Javascript, phpMyAdmin, React, MAMP, Bootstrap"
-                    />
-
-            <p><strong>Just call me awesome!</strong></p>
-        </div>
-        </div>
-        <div className="w3-half w3-brown w3-container w3-center" id="extracurricular">
-            <div className="w3-padding-64 w3-animate-right">
-            <h1><strong>Extra Extra!</strong></h1>
-
-                    <Icon icon={DSA} />
-
-                    <Thumbnail
-                    link=""
-                    title="Design and Analysis of Algorithms and Fun with Algorithms"
-                    category="Data Structures and Algorithms"
-                    />
-
-                    <Summary
-                    link="https://github.com/beauvilerobed/data-structures_and_algorithms"
-                    title="Topics from Stanford's & UCS's Algorithms and Data Structures course"
-                    category="Python3, unittest, Javascript, QUnit API"
-                    />
-
-                    <Summary
-                    link="https://github.com/beauvilerobed/data-structures_and_algorithms"
-                    title="Fun algorithm problems and data structure constructions that will keep your mind sharp!"
-                    category="Python3, unittest"
-                    />
-
-                    <Icon icon={learnSQL} />
-
-                    <Thumbnail
-                    link="https://pitt.zoom.us/rec/play/u5R_dbz--Do3Gd2X4gSDU_R9W9W_LPqs1SNM__IOzEfgW3ZRZAWgY7QbM7ZsjCo61KohPPaTmdLMf2oU?startTime=1588968239000&_x_zm_rtaid=bp79UOdnQVuamy3OCN4kYA.1590750115444.5c8cc25b682ee9418e7b3c1b891685f8&_x_zm_rhtaid=897"
-                    title="Let’s Learn SQL through Set Theory! Part 1"
-                    category="Computer Science and Mathematics Seminar"
-                    />
-
-                    <Summary
-                    link="https://github.com/beauvilerobed/Lets_learn_sql_webinar_1_and_2"
-                    title="Now, I know what you’re asking; what the heck is SQL? It stand s for Structured Query Language. It’s a stand ard computer lan- guage used for accessing  and manipulating databases. Wait! Manipulate databases? Computer language? I thought this was a math talk! Don’t worry this is a math talk and sort of. In this talk we will be learning the basics of a computer language (SQL) using mathematics (set theory). Whether you know some computer languages or none at all, it’s always fun to learn one, so let’s do that!"
-                    category="SQL, Microsoft Offic, Zoom"
-                    />
-
-                    <Icon icon={learnSQL2} />
-
-                    <Thumbnail
-                    link="https://pitt.zoom.us/rec/play/vpB5Ju6hrzI3HNadswSDBqctW9W8fams0yEa8vYPmk6wB3gGO1H0N7pHYbBk9jUj4awd6dVPRweaSm8y?startTime=1588795106000&_x_zm_rtaid=bp79UOdnQVuamy3OCN4kYA.1590750115444.5c8cc25b682ee9418e7b3c1b891685f8&_x_zm_rhtaid=897/"
-                    title="Let's Learn SQL through Set Theory! Part 2 Application "
-                    category="Computer Science and Mathematics Seminar"
-                    />
-
-                    <Summary
-                    link="https://github.com/beauvilerobed/Lets_learn_sql_webinar_1_and_2"
-                    title="One of the best ways to really learn about something is having a concrete example (or counter- example) of that something. In this talk we will walkthrough a con- crete example. We will see the ba- sics of SQL in action on a newly designed database  and other awe- some stuff that I won’t speak of at this moment. You’ll just have to come  and see for yourself."
-                    category="SQL, PHP, Microsoft Offic, Mamp, phpMyAdmin"
-                    />
-            </div>
-        </div>
-        </div>
-
+        <Projects/>
         <ContactMe />
     </div>
   )
