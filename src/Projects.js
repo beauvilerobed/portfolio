5dd9f20d7084b3cdeb60b62539c088203333f42d
Robed <robedbeauvil@gmail.com>
2020-11-13 21:13:04 -0500

    added more components

--- src/Projects.js
+++ src/Projects.js
@@ -1,45 +1,20 @@
 import React from "react"
 import Thumbnail from './Thumbnail.js';
 import Summary from './Summary.js';
+import ContactMe from './ContactMe.js'
+import AboutMe from './AboutMe.js';
 import Icon from './Images/ThumbnailImage';
 import sFITNESS from './Images/sFITNESS.jpg'
 import calc_bot from './Images/calc_bot.png'
 import learnSQL from './Images/learnSQL.png'
 import learnSQL2 from './Images/learnSQL2.png'
 import TWITTY from './Images/TWITTY.png'
-import resume from './Images/resume.png' 
 import DSA from './Images/DSA.jpg'
 
 function Projects(props) {
   return (
     <div>
-        <div className="w3-row w3-animate-right container1">
-        <div className="w3-half w3-black w3-container w3-center" style={{height: "700px"}}>
-            <div className="w3-padding-64">
-            <h1><strong>Robed Beauvile</strong></h1>
-            <div id="container" className="container-fluid"></div>
-            <h4>Welcome to my personal website</h4>
-            </div>
-            <div className="w3-padding-64 w3-animate-top">
-            <a href="#about" className="w3-button w3-black w3-block w3-hover-light-grey w3-padding-16">About Me</a>
-            <a href="#work" className="w3-button w3-black w3-block w3-hover-blue-grey w3-padding-16">Projects</a>
-            <a href="#extracurricular" className="w3-button w3-black w3-block w3-hover-brown w3-padding-16">Extracurricular</a>
-            <a href="#contact" className="w3-button w3-black w3-block w3-hover-dark-grey w3-padding-16">Contact</a>
-            <a href="#resume" className="w3-button w3-black w3-block w3-hover-light-grey w3-padding-16">Resume</a>
-            
-            </div>
-        </div>
-        <div className="w3-half w3-light-grey w3-container" style={{height: "700px"}} id="about">
-            <div className="w3-padding-64 w3-animate-left w3-center">
-            <h1><strong>About Me</strong></h1>
-            <div className="w3-left-align w3-padding-large">
-                <p>I am a disciplined and persistent problem-solver, open to new opportunities, and comfortable with being uncomfortable.</p>
-                <p>Highly motivated and well-versed in various programming languages, with a master's degree in Applied Mathematics and work experience in academia and tech. </p>
-            </div>
-            </div>
-        </div>
-        </div>
-
+        <AboutMe />
         <div className="w3-row w3-animate-left container2">
         <div className="w3-half w3-blue-grey and w3-center" id="work">
             <div className="w3-padding-64 w3-animate-right">
@@ -55,7 +30,7 @@ function Projects(props) {
 
                     <Summary
                     link="https://github.com/beauvilerobed/calctutor123"
-                    title="Artificial math tutor that helps you find derivatives  and indefinite integrals through a step-by-step process with optional conversation (if you are not sure about any of the steps."
+                    title="Artificial math tutor that helps you find derivatives  and indefinite integrals through a step-by-step process with optional c."
                     category="Python3, HTML/CSS, Javascript, Django, Bootstrap, D3.js, Keras, NLTK, Tensorflow, JSON, Sympy, Numpy"
                     />
 
@@ -146,31 +121,7 @@ function Projects(props) {
         </div>
         </div>
 
-        <div className="w3-row w3-animate-right container3" id="contact">
-        <div className="w3-half w3-dark-grey w3-container w3-center">
-            <div className="w3-padding-64 w3-animate-right">
-            <h1><strong>Contact</strong></h1>
-            </div>
-            <div className="w3-padding-64 w3-animate-left">
-            <p>..for a cup of coffee, or whatever.</p>
-            <p>Orlando, FL</p>
-            <p>(850) 354-0565</p>
-            <p>robedbeauvile001@gmail.com</p>
-            </div>
-        </div>
-        <div className="w3-half w3-light-grey w3-container" id="resume">
-        <div className="w3-padding-64 w3-animate-left w3-center">
-            <h1><strong>Resume</strong></h1>
-
-                    <Icon icon={resume} />
-
-                    <Thumbnail
-                    link="https://github.com/beauvilerobed/Robed-Beauvile/blob/master/Robed%20Beauvil2.pdf"
-                    title="See in Github"
-                    />
-            </div>
-        </div>
-        </div>
+        <ContactMe />
     </div>
   )
 }
