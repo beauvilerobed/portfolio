bb7e138ab31ad64ef22455d0a70755dd2e65fdf4
Robed <robedbeauvil@gmail.com>
2020-11-14 09:04:52 -0500

    modern buttons and thumbnails

--- src/content/ContactMe.js
+++ src/content/ContactMe.js
@@ -7,11 +7,10 @@ function ContactMe(props) {
     <div className="w3-row w3-animate-right container3" id="contact">
     <div className="w3-half w3-dark-grey w3-container w3-center">
         <div className="w3-padding-64 w3-animate-right">
-        <h1><strong>Contact</strong></h1>
+        <h1><strong>Contact Me</strong></h1>
         </div>
         <div className="w3-padding-64 w3-animate-top">
         <p>..for a cup of coffee, or whatever.</p>
-        <p>Orlando, FL</p>
         <p>(850) 354-0565</p>
         <p>robedbeauvile001@gmail.com</p>
         </div>
