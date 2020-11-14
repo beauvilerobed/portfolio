b12c8bdb0c91eac27178c44e8188620261a5df53
Robed <robedbeauvil@gmail.com>
2020-11-14 08:34:22 -0500

    added scrolling for links in homepage

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import { Link } from "react-scroll";
  
 function NavigationBar(props) {
   return (
@@ -7,14 +8,58 @@ function NavigationBar(props) {
             <div className="w3-padding-64">
             <h1><strong>Robed Beauvile</strong></h1>
             <div id="container" className="container-fluid"></div>
-            <h4>Welcome to my personal website</h4>
+            <h3><strong>Welcome!</strong></h3>
             </div>
             <div className="w3-padding-64 w3-animate-top">
-            <a href="#about" className="w3-button w3-black w3-block w3-hover-light-grey w3-padding-16">About Me</a>
-            <a href="#work" className="w3-button w3-black w3-block w3-hover-blue-grey w3-padding-16">Projects</a>
-            <a href="#extracurricular" className="w3-button w3-black w3-block w3-hover-brown w3-padding-16">Extracurricular</a>
-            <a href="#contact" className="w3-button w3-black w3-block w3-hover-dark-grey w3-padding-16">Contact</a>
-            <a href="#resume" className="w3-button w3-black w3-block w3-hover-light-grey w3-padding-16">Resume</a>
+              <Link
+                to='about'
+                spy={true}
+                smooth={true}
+                duration={500}
+                className='w3-button w3-black w3-block w3-hover-light-grey w3-padding-16'
+              >
+                About Me
+              </Link>
+
+              <Link
+                to='work'
+                spy={true}
+                smooth={true}
+                duration={500}
+                className='w3-button w3-black w3-block w3-hover-brown w3-padding-16'
+              >
+                Projects
+              </Link>
+
+              <Link
+                to='extracurricular'
+                spy={true}
+                smooth={true}
+                duration={500}
+                className='w3-button w3-black w3-block w3-hover-brown w3-padding-16'
+              >
+                Extracurricular
+              </Link>
+
+              <Link
+                to='contact'
+                spy={true}
+                smooth={true}
+                duration={500}
+                className='w3-button w3-black w3-block w3-hover-dark-grey w3-padding-16'
+              >
+                Contact
+              </Link>
+
+              <Link
+                to='resume'
+                spy={true}
+                smooth={true}
+                duration={500}
+                className='w3-button w3-black w3-block w3-hover-light-grey w3-padding-16'
+              >
+                Resume
+              </Link>
         </div>
     </div>
   )
