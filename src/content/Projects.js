10c077c97090da61466f667e45e07b7de3f80a94
beauvilerobed <robedbeauvil@gmail.com>
2022-02-27 18:21:08 -0500

    style: add new project content

--- src/content/Projects.js
+++ src/content/Projects.js
@@ -10,49 +10,61 @@ function Projects(props) {
     <div className="w3-half and w3-center" id="work">
         <div className="w3-padding-64 w3-animate-right">
             <h1><strong>Projects</strong></h1>
+            <h4><strong>From Machine learning to Web Development</strong></h4>
                 
                 <div id="calbot"/>
                 <Thumbnail
                 title="Calculus-Calculator"
-                category="E-learning Application"
+                category="E-learning Web Application"
                 />
                 <Summary
                 link="https://github.com/beauvilerobed/calctutor123"
-                title="Designed and launched a website illustrating a multi-step visualization of integrals and derivatives. Used a chatbot, built from scratch, to assist students with topics such as integrating by parts."
-                category="Python3, JavaScript, NLTK, Django, Keras, NumPy"
+                title=""
+                category="Python3, JavaScript, NLTK, Django, Keras, NumPy, Pandas"
                 />
 
                 <div id="starbucks"/>
                 <Thumbnail
                 title="Starbucks Rewards: Predicting Consumer Responses"
-                category="Machine Learning/Data Science"
+                category="Machine Learning/Data Science Project"
                 />
                 <Summary
                 link="https://github.com/beauvilerobed/predict-consumer-responses"
-                title="Utilized Extreme Gradient Boosting and Data Mining techniques to optimize promotion targeting for Starbucks' simulated data."
+                title=""
                 category="Python3, AWS Sagemaker, S3, XGBoost, pandas, scikit-learn"
                 />
 
                 <div id="dogbreeds" />
                 <Thumbnail 
                 title="Dog Breeds Image Classification"
-                category="Machine Learning/Deep Learning"
+                category="Machine Learning/Deep Learning Project"
                 />
                 <Summary
                 link="https://github.com/beauvilerobed/dog-breed-image-classification"
-                title="Applied profiling, debugging, and hyperparameter tuning to refine the performance of a convolutional neural network for image classification."
+                title=""
                 category="Python3, AWS Sagemaker, S3, Pytorch, matplotlib"
                 />
 
                 <div id="pyMatrix" />
                 <Thumbnail 
-                title="PyMatrix-package"
+                title="PyMatrix"
                 category="OOP/Python Package"
                 />
                 <Summary
                 link="https://github.com/beauvilerobed/py-matrix-package"
-                title="Retrieves matrix dimensions, their trace, and performs matrix addition, subtraction, multiplication, and scalar multiplication."
+                title=""
                 category="Python3"
+                />
+
+                <div id="sFITNESS" />
+                <Thumbnail 
+                title="sFITNESS"
+                category="Automated Exercise and Diet Scheduler"
+                />
+                <Summary
+                link="https://github.com/beauvilerobed/fullstack-fitness-trainer"
+                title=""
+                category="PHP, CSS3, JavaScript (React.js)"
                 />
             <p><strong>Just call me awesome!</strong></p>
     </div>
