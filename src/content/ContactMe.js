f52cfa83e016b9ecc056c06feab3324a18aae593
beauvilerobed <robedbeauvil@gmail.com>
2022-03-05 11:40:04 -0500

    task: add animation to contact section

--- src/content/ContactMe.js
+++ src/content/ContactMe.js
@@ -48,8 +48,23 @@ class ContactMe extends React.Component {
                     </ul>
 
                     <div id="cf">
-                        <img className="bottom" src={require("../img/calc_bot.png")} alt=""/>
-                        <img className="top" src={require("../img/robed_pic_work.jpg")} alt=""/>
+                        <img src={require("../img/calc_bot.png")} alt=""/>
+                        <img src={require("../img/robed_pic_work.jpg")} alt=""/>
+                        <img src={require("../img/automobiles.jpeg")} alt=""/>
+                        <img src={require("../img/bike_sharing.png")} alt=""/>
+                        <img src={require("../img/cNN.png")} alt=""/>
+                        <img src={require("../img/download_1.png")} alt=""/>
+                        <img src={require("../img/DSA.png")} alt=""/>
+                        <img src={require("../img/hangman.jpeg")} alt=""/>
+                        <img src={require("../img/learnSQL.png")} alt=""/>
+                        <img src={require("../img/learnSQL2.png")} alt=""/>
+                        <img src={require("../img/logo2.png")} alt=""/>
+                        <img src={require("../img/microblog.jpeg")} alt=""/>
+                        <img src={require("../img/ml_workflow.png")} alt=""/>
+                        <img src={require("../img/operationalize.png")} alt=""/>
+                        <img src={require("../img/search_image.png")} alt=""/>
+                        <img src={require("../img/sFITNESS.jpeg")} alt=""/>
+                        <img src={require("../img/robed_pic.jpg")} alt=""/>
                     </div>
         </div>
     </div> 
