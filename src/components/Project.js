283a5e8d046515088a736520ac421982a576ea18
beauvilerobed <robedbeauvil@gmail.com>
2024-08-21 20:45:14 -0400

    recenter stuff

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,8 +4,8 @@ function Project(props) {
   return (
     <div className="project">
       <div className="center-div">
-        <div id={props.pictureId}/>
           <div className="summary">
+          <em id={props.pictureId}/>
           <h3>{props.headline}</h3>
           <h2>
           <a href={props.link} className="et-white"><button className="tm-btn"><strong>GitHub</strong></button></a>
@@ -13,7 +13,8 @@ function Project(props) {
           <p> <strong className={(props.dontShowSummary) ? "hide-this" : ""}>Project includes:</strong>{props.summary} 
           </p>
           <p>
-          <strong className={(props.dontShowTools) ? "hide-this" : ""}>Tools:</strong>{props.tools}</p>
+          <strong className={(props.dontShowTools) ? "hide-this" : ""}>Tools:</strong>{props.tools}
+          </p>
           </div>
       </div>
     </div> 
